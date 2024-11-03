715e82d5c981b0a8a86fc23fc9328f5a4ba83076
renatomedeiros <renatomedeiros2001@gmail.com>
2024-11-03 15:30:06 +0000

    Adding a product details Screen!

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,23 +1,30 @@
-import { StyleSheet, Image } from 'react-native';
+import { StyleSheet, Image, Pressable } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 import Colors from '@/constants/Colors';
 import { Product } from '../types';
+import { Link } from 'expo-router';
 
 export const defaultPizzaImage = 
 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png';
 
 type ProductListItemProps = {
-    product: Product 
+    product: Product;
 }
 const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
-    <View style={styles.container}>
-      <Image source={{uri: product.image || defaultPizzaImage}} style={styles.image} />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>{product.price}€</Text>
-    </View>
+    // Since we are wrapping all the components we need to pass asChild so it maintain the original styles
+    // The link require a child with a onPress Event! thats why i replaced the View with the Pressable 
+    // the way to pass Dynamic properties is like the code bellow -> (`/${product.id}`)
+    <Link href={`/menu/${(product.id)}`} asChild>
+      <Pressable style={styles.container}>
+        {/* Since the image always try to fill 100% of the space, we may lost some part of the images.. so resizeMode='contain' will prevent that! */}
+        <Image source={{uri: product.image || defaultPizzaImage}} style={styles.image} resizeMode='contain' />
+        <Text style={styles.title}>{product.name}</Text>
+        <Text style={styles.price}>{product.price}€</Text>
+      </Pressable>
+    </Link>
   )
 }
 
@@ -25,9 +32,11 @@ export default ProductListItem //this is needed to really be able to export our 
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'black', //change to white in the white mode!
+    backgroundColor: 'white', //change to white in the white mode!
     padding: 10,
     borderRadius: 20,
+    flex: 1, //split equally the space!
+    maxWidth: '50%' //since we can have even products! and it should slit in 2 columns
   },
   title: {
     fontSize: 18, 
